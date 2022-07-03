5041efd9a9e40416330d56c8760edc9fd6732296
Cela24k <882778@stud.unive.it>
2022-07-03 16:34:06 +0200

    including js files

---
+++ poke-dex/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+    return (
+        <div className="App">
+            <Button>submit</Button>
+        </div>
+    );
+}
+
+export default App;
