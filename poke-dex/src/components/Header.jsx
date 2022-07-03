20aa8782878ddb5a409691bf0b2dc535282c8de0
Cela24k <882778@stud.unive.it>
2022-07-03 18:32:16 +0200

    Binded front end and http client

--- poke-dex/src/components/Header.jsx
+++ poke-dex/src/components/Header.jsx
@@ -1,9 +1,19 @@
 import '../scss/_header.scss';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export function Header(){
+    const [name,setName] = useState([]);
+    
+    function getPoke(){
+        fetch('https://pokeapi.co/api/v2/pokemon/1/')
+        .then((response)=>response.json())
+        .then((data)=> setName(data.forms[0].name));
+    }
+
+    useEffect(()=>{getPoke()});
+    
     return (
-        <div>Header</div>
+        <div>{name}</div>  
     );
 }
-
