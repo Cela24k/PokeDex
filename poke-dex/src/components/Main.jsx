d32d0643a8aea3f710ad3f86a0572a5f79b5bbda
emanueleattina1 <89386258+emanueleattina1@users.noreply.github.com>
2022-07-14 19:53:05 +0200

    fix get api problem

--- poke-dex/src/components/Main.jsx
+++ poke-dex/src/components/Main.jsx
@@ -1,52 +1,35 @@
 import '../scss/_main.scss';
 import axios from 'axios';
+import { SinglePokemon } from './SinglePokemon'
 import React, { useState, useEffect } from 'react';
-import { SinglePokemon } from './SinglePokemon';
 
-var next = 'https://pokeapi.co/api/v2/pokemon';
+export function Main(){
 
-export function Main() {
     const [pokemonList, setPokemonList] = useState([]);
-    const [loading,setLoading] = useState(false);
+    const url = "https://pokeapi.co/api/v2/pokemon/";
 
-    useEffect(()=>{
-        async function loadPoke() {return await populateList()};
-        setLoading(true);
-
-        loadPoke().then((e)=>{
-            setPokemonList(e);
-            setLoading(false);
-            console.log(e);
-        }).catch((e)=> {
-            setLoading(false);
-            console.log('Error '+e)
+    useEffect(() => {
+        axios.get(url)
+            .then((res) => {
+                const responsePokemon = res.data.results;
+                setPokemonList(responsePokemon);
+                url = res.data.next;
         });
     }, []);
-
-    return !loading?(
+    
+    return (
         <main className="container">
             <h1>Er Pokédex</h1>
-            <SinglePokemon name="Niggamon"/>
-            {pokemonList.map((e,idx)=><SinglePokemon name={e.name} key={idx} />)}
+            <div className='row mt-4'>
+            {
+                pokemonList.map((pokemon) => {
+                    console.log(pokemon);
+                    return (
+                        <SinglePokemon key={pokemon.id} name={pokemon.name} />
+                    )
+                })
+            }
+            </div>
         </main>
-    ):(
-        <div>Caricamento...</div>)
-}
-
-async function populateList() {
-    let list = [];
-    axios.get(next)
-        .then((res) => {
-            next = res.data.next;
-            res.data.results.map((e) => {
-
-                return axios.get(e.url)
-                    .then((res) => {
-                        list[res.data.id] = res.data;
-                    })
-                    .catch((e)=>{console.log('error '+e+' while loading a pokemon')})
-            })
-        })
-        .catch((e)=>{console.log('error '+e+' while loading the list')})
-    return list;
+    );
 }
